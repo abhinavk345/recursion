c7b86377d5a6bc7ba962af1ed777d124496209e1
Abhinav Kumar <abhinav.kumar@intelliswift.com>
2023-08-03 14:43:47 +0530

    new file added

--- src/com/abhi/recursion/CheckArraySorted.java
+++ src/com/abhi/recursion/CheckArraySorted.java
@@ -1,2 +1,22 @@
-package com.abhi.recursion;public class CheckArraySorted {
+package com.abhi.recursion;
+
+public class CheckArraySorted {
+
+    public static boolean checkArraySorted(int arr[], int index) {
+        if (index == arr.length - 1) {
+            return true;
+        }
+        if (arr[index] < arr[index + 1]) {
+            return checkArraySorted(arr, index + 1);
+
+        } else {
+            return false;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 5, 16, 117, 338};
+
+        System.out.println(checkArraySorted(arr, 0));
+    }
 }
